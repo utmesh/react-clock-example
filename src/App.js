c4ec09ac9492b1d628827800dd0dd5674a6ba996
Umesh Thapa <utmesh5@gmail.com>
2020-04-27 11:49:51 +0200

    todo updated

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 import Ghadi from './Ghadi.js'
 import Header from './Header.js'
-//import Circle from './Circle.js'
+
 
 
 
 export default function App() {
   return (
-    <div>
+    <>
       <Header />
       <Ghadi />
-    </div>
+    </>
   )
 }
--- src/Ghadi.js
+++ src/Ghadi.js
@@ -1,80 +1,57 @@
 import React, { useState, useEffect } from "react";
 import Switch from "react-switch"
-import { StyleSheet, View, Text} from 'react-native';
-import {FaCalendarAlt} from 'react-icons/fa';
+import { FaCalendarAlt } from 'react-icons/fa';
 
 
 import "./Ghadi.css";
 
 
-export default function Ghadi(){
+export default function Ghadi() {
   const options = { year: 'numeric', month: 'long', day: '2-digit', weekday: 'short' }
   const [clock, setClock] = useState(new Date().toLocaleTimeString());
   const [tarik, setTarik] = useState(new Date().toLocaleDateString("en-IN", options))
-  const[checked,  setChecked] = useState(true)
+  const [checked, setChecked] = useState(true)
 
   useEffect(() => {
     setInterval(tick, 1000)
   });
 
   useEffect(() => {
-    setTarik(new Date().toLocaleDateString("en-IN",options))
+    setTarik(new Date().toLocaleDateString("en-IN", options));
   })
 
-  
 
-  const toggle = ()=>{
-      setChecked(!checked)
+
+  const toggle = () => {
+    setChecked(!checked)
   }
 
   function tick() {
     return setClock(new Date().toLocaleTimeString())
   }
 
-  const styles = StyleSheet.create(
-    {
-        CircleShape: {
-            width: 250,
-            height: 250,
-            alignSelf: "center",
-            borderRadius: 250 / 2,
-            backgroundColor: 'darkgrey',
-            borderEndColor:'black'
-            
-        },
-        Display:{
-            textAlign:"center",
-            marginTop: 50,
-            fontSize:20,
-            color: 'blue',
-            
-        }
-
-    }
-)
-
-  
+//TODO: convert view and text to html tags. instead of inline style use class/css
 
   return (
-    <div className="GhadiBody">
-        <div className='switchAndCalendar'>
-            <Switch onChange ={toggle} checked ={checked}/>
-            <FaCalendarAlt size="30"/>
-        </div>
+    <div className='GhadiBody'>
+      <div className='switchAndCalendar'>
+        <Switch onChange={toggle} checked={checked} />
+        <FaCalendarAlt size="30" />
+      </div>
 
-        <View style={styles.CircleShape}>
-            <Text style={styles.Display}>
-                <h1>{clock}</h1>
-                {checked && (<h4>{tarik}</h4>)}
-            </Text>    
-        </View>
+      <div className='circleStyle'> 
+        <div className='textDisplay'>
+          <h1>{clock}</h1>
+          {checked && (<h4>{tarik}</h4>)}
+        </div>
+      </div>
     </div>
   );
 }
 
 
 
-       
-            
-        
+
+
+
 
